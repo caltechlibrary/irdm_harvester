8441990c9367b0be81024f1b2e154ab631f4d2f3
Tom Morrell <tmorrell@caltech.edu>
2024-01-22 16:39:24 -0800

    Error reporting

--- save_dois.py
+++ save_dois.py
@@ -6,6 +6,7 @@ result = json.loads(sys.argv[1])
 dois = result['doi']
 
 for doi in dois.keys():
+    print(dois[doi])
     if dois[doi] != 'null':
         with open("harvested_dois.txt", "a") as f:
             f.write(doi + "\n")
