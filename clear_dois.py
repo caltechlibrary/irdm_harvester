2fcf0dd6e1d7cc4ccdfd0e176d3e97cf0d64e6f7
Tom Morrell <tmorrell@caltech.edu>
2025-07-29 15:24:59 -0700

    Prep for reload of articles

---
+++ clear_dois.py
@@ -0,0 +1,16 @@
+import csv
+
+infile = "to_close.csv"
+
+with open(infile, 'r') as f:
+    reader = csv.reader(f)
+    dois = [row[3] for row in reader if row]  
+
+with open("harvested_dois.txt") as infile:
+    harvested_dois = infile.read().splitlines()
+
+for doi in harvested_dois:
+    if doi in dois:
+        harvested_dois.remove(doi)
+with open("harvested_dois.txt", 'w') as outfile:
+    outfile.write('\n'.join(harvested_dois))
