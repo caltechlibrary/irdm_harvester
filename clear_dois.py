4621208086d8920836b8819b7ad945ed0c832c41
Tom Morrell <tmorrell@caltech.edu>
2025-07-29 16:29:51 -0700

    Formatting

--- clear_dois.py
+++ clear_dois.py
@@ -2,9 +2,9 @@ import csv
 
 infile = "to_close.csv"
 
-with open(infile, 'r') as f:
+with open(infile, "r") as f:
     reader = csv.reader(f)
-    dois = [row[3] for row in reader if row]  
+    dois = [row[3] for row in reader if row]
 
 with open("harvested_dois.txt") as infile:
     harvested_dois = infile.read().splitlines()
@@ -13,5 +13,5 @@ for doi in dois:
     if doi in harvested_dois:
         harvested_dois.remove(doi)
 
-with open("harvested_dois.txt", 'w') as outfile:
-    outfile.write('\n'.join(harvested_dois))
+with open("harvested_dois.txt", "w") as outfile:
+    outfile.write("\n".join(harvested_dois))
